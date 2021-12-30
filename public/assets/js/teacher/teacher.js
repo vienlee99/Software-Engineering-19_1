4ff990496c819f4d8ea2fbc49eb79f980db6eaad
NguyenCatTuong <nct542@gmail.com>
2021-12-30 12:30:15 +0700

    update teacher/upload

---
+++ public/assets/js/teacher/teacher.js
@@ -0,0 +1,15 @@
+
+function preview() {
+    frame.src = URL.createObjectURL(event.target.files[0]);
+}
+function clearImage() {
+    document.getElementById('formFile').value = null;
+    // document.getElementById('formfile1').value = null;
+    frame.src = "";
+    frame1.src = "";
+}
+
+
+function preview1() {
+    frame1.src = URL.createObjectURL(event.target.files[0]);
+}
